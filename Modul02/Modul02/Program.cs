7949717bcb3f2315dfba7d5f8bda4ad9e0de192f
nikolapenev89 <nikola.penev.89@abv.bg>
2016-01-07 00:45:16 +0200

    Модул 02, Урок 02 - Променливи int

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -10,9 +10,24 @@ namespace Modul02
 
 			// Дефиниране на променливи
 
+			int a = 1, b = 2;
+			int sum = a + b;
+
 			// Събиране на числа
 
+			a = 55;
+			b = 19;
+
+			sum = a + b;
+
 			// Писане в конзолата
+
+			Console.Write (a);
+			Console.Write ("+");
+			Console.Write (b);
+			Console.Write ("=");
+			Console.WriteLine (sum);
+
 		}
 	}
 }
