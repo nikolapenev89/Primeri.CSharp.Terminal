31d92c74bb62152fbec46eb45ab0b9c2b3207c32
nikolapenev89 <nikola.penev.89@abv.bg>
2016-01-11 22:24:32 +0200

    Модул 02, Урок 03 - Основни оператори за целочислен тип данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -5,18 +5,19 @@ namespace Modul02
 	class MainClass
 	{
 		public static void Main (string[] args)
+
 		{
 
 
 			// Дефиниране на променливи
 
-			int a = 1, b = 2;
-			int sum = a + b;
+			int a = 0, b = 0;
+			int sum = 0;
 
 			// Събиране на числа
 
-			a = 55;
-			b = 19;
+			a = 10;
+			b = 5;
 
 			sum = a + b;
 
@@ -28,6 +29,27 @@ namespace Modul02
 			Console.Write ("=");
 			Console.WriteLine (sum);
 
+			//Допълнителни оператори
+
+			Console.WriteLine ("\nИзползване на +=");
+			a += b;
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine (a);
+
+			Console.Write ("\nИзползване на ++");
+			a++;
+			Console.WriteLine (a);
+
+			Console.WriteLine ("Стандартно деление: " + (7 / 4).ToString ()); // -> 2 (7/3 = 2,3333)
+			Console.WriteLine ("Остатък от деление: " + (7 % 3).ToString ()); // -> 1 (1/3 = 0,3333)
+           
 		}
 	}
 }
