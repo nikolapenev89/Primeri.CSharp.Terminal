fb119634c9b7ed182ad075cbb0d3c862df5164ab
nikolapenev89 <nikola.penev.89@abv.bg>
2016-01-17 14:38:47 +0200

    Модул 02, Урок 08

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -6,7 +6,34 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
-			string[] row = new string[3];
+		    //Дефиниране на таблица
+
+			string[,] table = new string[2, 3];
+
+			//Въвеждане на първи ред
+
+			table [0, 0] = "Никола"; table [0, 1] = "Пенев"; table [0, 2] = "26";
+
+			//Въвеждане на втори ред
+
+			table [1, 0] = "Вероника"; table [1, 1] = "Ханджиева"; table [1, 2] = "22";
+
+			//Печат
+
+			Console.Write ("Кой ред искате да видите?");
+
+			int _index = Convert.ToInt32 (Console.ReadLine ())-1;
+
+			Console.WriteLine(table[_index, 0][0]+". " + table[_index, 1] + ", " + table[_index, 2]+" г.");
+				
+		}
+	}
+}
+/*
+
+		    string[] row = new string[3];
+		
+	
 
 			row [0] = "Ред 1";
 			row [1] = "Ред 2";
@@ -15,10 +42,9 @@ namespace TableProject
 			Console.WriteLine ("Масив row: " + row[0] + ", "+ row[1] + ", "+ row[1] + "\n");
 
 			string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
-			Console.WriteLine ("Големината на масива \n " +"1, 2, 3, 4, 5 ,6, 7, 8, 9\n\ne: "+parse.Length);
+			Console.WriteLine ("Големината на масива \n " + "1, 2, 3, 4, 5 ,6, 7, 8, 9\n\ne: " + parse.Length);
 
-			string list1 = string.Join (";", parse);
-			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
-		}
-	}
-}
+			string list1 = string.Join (";", parse + "\n\n");
+			Console.WriteLine ("Новият стринг е:\n" + list1);
+
+            */
